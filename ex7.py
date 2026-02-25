9bfe146c62148e2108934221501727b847397b7f
Erik Trindade <erik.trindade363@gmail.com>
2026-02-25 01:01:22 -0300

    ex7.py

---
+++ ex7.py
@@ -0,0 +1,14 @@
+#Exercicio Simples
+
+#Lista com For
+
+tarefas = []
+
+tarefas.append('Estudar Python')
+tarefas.append('Estudar Russo')
+tarefas.append('Estudar Ingles')
+tarefas.append('Treinar Boxe')
+
+print('Minhas tarefas de hoje: ')
+for tarefa in tarefas:
+    print(f'Tarefa: {tarefa}')
